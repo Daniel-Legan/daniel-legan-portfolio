8fe253dd0574cd20f28a6455836f100d29714a41
Daniel Legan <legan.daniel@gmail.com>
2023-11-20 15:50:59 -0500

    add download resume button

--- portfolio/src/components/ContactForm.js
+++ portfolio/src/components/ContactForm.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const ContactForm = () => {
+    const resumeUrl = process.env.PUBLIC_URL + '/resume/Legan Daniel Resume.pdf';
+
     return (
         <div className="contact-form">
             <h2>Contact Me</h2>
+            <a href={resumeUrl} download="Legan_Daniel_Resume.pdf">
+                <button>Download Resume</button>
+            </a>
         </div>
     );
 };
