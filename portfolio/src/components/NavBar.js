2c0f1f5205c075c790cd133cbaf65030597d9fe3
Daniel Legan <legan.daniel@gmail.com>
2023-11-20 13:05:09 -0500

    add links next

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -4,7 +4,6 @@ const NavBar = () => {
     return (
         <nav className="navbar">
             <h1>My GitHub Portfolio</h1>
-            {/* Add more navigation links as needed */}
         </nav>
     );
 };
